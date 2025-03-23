5694f709a683869b2f61703bd676df97a52ad8bf
Aryan Tiwari <aryan.tiwari_cs23@gla.ac.in>
2025-03-23 14:37:42 +0530

    initialize React app with Vite and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-red-500'>App</div>
+  )
+}
+
+export default App
+
+
+
+/*
+
+git add README.md
+git@github.com:Aryantiwarib/EventX.git
+git push -u origin main
+*/
